c83bd026ce258dd39e8b20c603493a4323893451
CarlosG18 <c.gabriel.abc18@gmail.com>
2023-12-07 22:12:07 -0300

    adicionando a base do projeto - front end

--- dev/barbershop/agendamento/urls.py
+++ dev/barbershop/agendamento/urls.py
@@ -4,5 +4,13 @@ from . import views
 app_name = "agendamento"
 
 urlpatterns = [
-    path('', views.index, name='agendamento'),
+    path('', views.index, name='index'),
+    path('horarios/', views.horarios, name='horarios'),
+    path('gerente/', views.home_gerente, name='home_gerente'),
+    path('gerente/cadastro/barber', views.cadastro_barber, name='cadastro_barber'),
+    path('gerente/new_service/', views.new_service, name='new_service'),
+    path('gerente/barbeiros/', views.barbeiros, name='barbeiros'),
+    path('gerente/services', views.services, name='services'),
+    path('agendamentos/', views.agendamentos, name='agendamentos'),
+    path('perfil/', views.perfil, name='perfil'),
 ]
--- dev/barbershop/agendamento/views.py
+++ dev/barbershop/agendamento/views.py
@@ -2,3 +2,35 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'agendamento/index.html')
+
+def horarios(request):
+    minutos = ['00', '30']
+    horarios = []
+    for i in range(7, 22):
+        for j in range(2):
+            horarios.append(f'{i}:{minutos[j]}')
+
+    return render(request, 'agendamento/data.html',{
+        "horarios": horarios,
+    })
+
+def home_gerente(request):
+    return render(request, 'agendamento/gerente/home.html')
+
+def cadastro_barber(request):
+    return render(request, 'agendamento/gerente/cadastro_barber.html')
+
+def new_service(request):
+    return render(request, 'agendamento/gerente/new_service.html')
+
+def barbeiros(request):
+    return render(request, 'agendamento/gerente/list_barber.html')
+
+def services(request):
+    return render(request, 'agendamento/gerente/list_services.html')
+
+def agendamentos(request):
+    return render(request, 'agendamento/agendamentos.html')
+
+def perfil(request):
+    return render(request, 'agendamento/perfil.html')
